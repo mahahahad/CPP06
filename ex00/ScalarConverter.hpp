303c1f54d2c0fe923b33a62a36505495e7f44885
mahahahad <mahad.abd2003@gmail.com>
2025-04-15 20:01:52 +0400

    style: move public access level to the top of the class

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -10,18 +10,18 @@ class   ScalarConverter {
     public:
         static int  convert( const std::string& ref );
         enum    Type { E_INT, E_DOUBLE, E_FLOAT, E_CHAR };
-
-    private:
-        ScalarConverter( void );
-        ScalarConverter( const ScalarConverter& copy );
-        ScalarConverter&  operator=( const ScalarConverter& copy );
-        ~ScalarConverter( void );
     
     public:
         class   ConversionException: public std::exception {
             public:
                 virtual const char * what() const throw();
         };
+
+    private:
+        ScalarConverter( void );
+        ScalarConverter( const ScalarConverter& copy );
+        ScalarConverter&  operator=( const ScalarConverter& copy );
+        ~ScalarConverter( void );
 };
 
 #endif
